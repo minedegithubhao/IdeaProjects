dd27777c56de1a2ecffbc34df3e25a7862cca599
minedegithubhao <824737880@qq.com>
2023-09-25 14:04:07 +0800

    m

---
+++ spring-book/spring-book-ch4/configuring-and-using-jdbctemplate/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.sql.SQLException;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+	public static void main(String[] args) throws SQLException {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Ch4Configuration.class);
+		
+		AccountDao accountDao = applicationContext.getBean(AccountDao.class);
+		
+	}
+}
--- spring-book/spring-book-ch4/readme.md
+++ spring-book/spring-book-ch4/readme.md
@@ -4,6 +4,8 @@
 该示例演示了Spinrgmvc对嵌入式数据库的支持数据库
 ## [configuring-datasource-pool](configuring-datasource-pool)[pooled-datasource](pooled-datasource)
 该示例演示了H2数据库连接池
-
-
+## [configuring-and-using-jdbctemplate](configuring-and-using-jdbctemplate)
+该示例演示了如何配置JdbcTemplate
+## [running-queries-with-jdbctemplate](running-queries-with-jdbctemplate)
+该示例演示了如何使用JdbcTemplate进行查询
 
