2b5069ed5465c6de866f897b6cbb7cdf3451aeae
minedegithubhao <824737880@qq.com>
2023-10-06 09:50:36 +0800

    m

--- spring-book/spring-book-ch11/basicwithannotations/src/main/java/org/example/config/AppConfig.java
+++ spring-book/spring-book-ch11/basicwithannotations/src/main/java/org/example/config/AppConfig.java
@@ -8,8 +8,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * Created by mertcaliskan
  * on 28/09/14.
  */
+//@Configuration等价于<beans>
 @Configuration
+//@ComponentScan(basePackages = {"org.example"})等价于<context:component-scan base-package="org.example" />
 @ComponentScan(basePackages = {"org.example"})
+//@EnableWebMvc等价于<mvc:annotation-driven />
 @EnableWebMvc
 public class AppConfig {
 }
