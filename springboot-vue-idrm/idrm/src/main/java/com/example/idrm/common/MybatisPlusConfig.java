13a6ca74a42ab2fbb310d1d9ba8360960e47eaa9
canbe <11183018+minedegiteehao@user.noreply.gitee.com>
2024-03-25 14:46:45 +0800

    fix(分页插件)

---
+++ springboot-vue-idrm/idrm/src/main/java/com/example/idrm/common/MybatisPlusConfig.java
@@ -0,0 +1,32 @@
+package com.example.idrm.common;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author cxdpc
+ * @date 2024/3/25 11:19
+ */
+@Configuration
+@MapperScan("com.example.idrm.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 添加分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setDbType(DbType.MYSQL);
+        paginationInnerInterceptor.setOverflow(true);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
+    }
+
+}
