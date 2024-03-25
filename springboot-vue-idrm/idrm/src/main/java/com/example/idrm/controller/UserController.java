5a7f05035de332eb5abe8b0648525e08c9df975f
canbe <11183018+minedegiteehao@user.noreply.gitee.com>
2024-03-25 10:33:20 +0800

    fix(mybatis-plus-generator改增删改查)

--- springboot-vue-idrm/idrm/src/main/java/com/example/idrm/controller/UserController.java
+++ springboot-vue-idrm/idrm/src/main/java/com/example/idrm/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.example.idrm.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.idrm.entity.User;
 import com.example.idrm.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +26,38 @@ public class UserController {
     @GetMapping("/list")
     public List<User> list(){
         return userService.list();
+    }
+
+    @GetMapping("/getUserById/{id}")
+    public User getUserById(@PathVariable Integer id){
+        return userService.getById(id);
+    }
+
+    @PostMapping("listCondition")
+    public List<User> listCondition(@RequestBody User user){
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(User::getName,user.getName());
+        return userService.list(queryWrapper);
+    }
+
+    @PostMapping("/save")
+    public boolean save(@RequestBody User user){
+        return  userService.save(user);
+    }
+
+    @PostMapping("/update")
+    public boolean update(@RequestBody User user){
+        return userService.updateById(user);
+    }
+
+    @PostMapping("/saveOrUpdate")
+    public boolean saveOrUpdate(@RequestBody User user){
+        return userService.saveOrUpdate(user);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public boolean delete(@PathVariable Integer id){
+        return userService.removeById(id);
     }
 
 }
