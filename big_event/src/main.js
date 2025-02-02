f87a6d5b5efd4383757534a482e3165d35c2bc02
minedegithubhao <824737880@qq.com>
2025-02-02 12:54:07 +0800

    空的Vue项目

---
+++ big_event/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.scss' // 引入scss
+import { createApp } from 'vue' // 引入vue
+import ElementPlus from 'element-plus' // 引入element-plus
+import 'element-plus/dist/index.css' // 引入element-plus样式
+import App from './App.vue' // 引入App.vue
+
+const app = createApp(App) // 创建vue实例
+
+app.use(ElementPlus) // 使用element-plus
+app.mount('#app') // 挂载vue实例
