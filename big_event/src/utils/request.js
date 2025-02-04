2005774a01eb76bd972417fb51c1c78723f52ffe
minedegithubhao <824737880@qq.com>
2025-02-04 19:51:17 +0800

    feat:统一处理未登录

--- big_event/src/utils/request.js
+++ big_event/src/utils/request.js
@@ -4,6 +4,7 @@
 import axios from 'axios';
 import { ElMessage } from 'element-plus'
 import { useTokenStore } from '@/stores/token.js' //导入token
+import router from '@/router/index.js'
 
 //定义一个变量,记录公共的前缀  ,  baseURL
 const baseURL = '/api';
@@ -14,7 +15,7 @@ instance.interceptors.request.use(
     config => {
         //在请求头中添加token
         let tokenStore = useTokenStore()
-        if(tokenStore.token){
+        if (tokenStore.token) {
             config.headers.Authorization = tokenStore.token;
         }
         return config;
@@ -28,14 +29,21 @@ instance.interceptors.request.use(
 //添加响应拦截器
 instance.interceptors.response.use(
     result => {
-        if(result.data.code ===0){
+        if (result.data.code === 0) {
             return result.data;
         }
         ElMessage.error(result.msg ? result.msg : '服务异常')
         return Promise.reject(result.data);//异步的状态转化成失败的状态
     },
     err => {
-        ElMessage.error('服务异常')
+        // 如果响应码是401未授权，则跳转到登录页面
+        if (err.response.status === 401) {
+            //跳转到登录页面
+            ElMessage.error('请先登录！')
+            router.push('/login')
+        } else {
+            ElMessage.error('服务异常')
+        }
         return Promise.reject(err);//异步的状态转化成失败的状态
     }
 )
