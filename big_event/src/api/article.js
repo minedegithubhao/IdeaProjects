450c25d36454a9c785cf84a62ed422b8ad97558a
minedegithubhao <824737880@qq.com>
2025-02-05 20:20:28 +0800

    feat:添加文章分类

--- big_event/src/api/article.js
+++ big_event/src/api/article.js
@@ -1,8 +1,13 @@
 //导入request.js中的请求工具
 import request from '@/utils/request'
 
-//注册接口函数
+//获取列表接口函数
 export const articleCategoryListService = () => {
     // 会以json的形式传递参数
     return request.get('/category')
+}
+
+export const addArticleCategoryService = (categoryModel) => {
+    // 以post请求方式添加文章分类
+    return request.post('/category', categoryModel)
 }
--- big_event/src/views/article/ArticleCategory.vue
+++ big_event/src/views/article/ArticleCategory.vue
@@ -4,7 +4,8 @@ import {
     Delete
 } from '@element-plus/icons-vue'
 import { ref } from 'vue'
-import { articleCategoryListService } from '@/api/article'
+import { ElMessage } from 'element-plus'
+import { articleCategoryListService, addArticleCategoryService } from '@/api/article'
 const categorys = ref([
     {
         "id": 3,
@@ -35,6 +36,43 @@ const articleCategoryList = async () => {
     }
 }
 articleCategoryList()
+
+//控制添加分类弹窗
+const dialogVisible = ref(false)
+
+//添加分类数据模型
+const categoryModel = ref({
+    categoryName: '',
+    categoryAlias: ''
+})
+//添加分类表单校验
+const rules = {
+    categoryName: [
+        { required: true, message: '请输入分类名称', trigger: 'blur' },
+    ],
+    categoryAlias: [
+        { required: true, message: '请输入分类别名', trigger: 'blur' },
+    ]
+}
+
+//添加分类
+const addArticleCategory = async () => {
+    let result = await addArticleCategoryService(categoryModel.value)
+    ElMessage.success(result.msg ? result.msg : '添加成功')
+
+    // 刷新列表
+    articleCategoryList()
+
+    // 关闭弹窗
+    dialogVisible.value = false
+
+    // 清空model中的数据
+    categoryModel.value = {
+        categoryName: '',
+        categoryAlias: ''
+    }
+    
+}
 </script>
 <template>
     <el-card class="page-container">
@@ -42,7 +80,7 @@ articleCategoryList()
             <div class="header">
                 <span>文章分类</span>
                 <div class="extra">
-                    <el-button type="primary">添加分类</el-button>
+                    <el-button type="primary" @click="dialogVisible = true">添加分类</el-button>
                 </div>
             </div>
         </template>
@@ -52,7 +90,7 @@ articleCategoryList()
             <el-table-column label="分类别名" prop="categoryAlias"></el-table-column>
             <el-table-column label="操作" width="100">
                 <template #default="{ row }">
-                    <el-button :icon="Edit" circle plain type="primary" ></el-button>
+                    <el-button :icon="Edit" circle plain type="primary"></el-button>
                     <el-button :icon="Delete" circle plain type="danger"></el-button>
                 </template>
             </el-table-column>
@@ -60,6 +98,24 @@ articleCategoryList()
                 <el-empty description="没有数据" />
             </template>
         </el-table>
+
+        <!-- 添加分类弹窗 -->
+        <el-dialog v-model="dialogVisible" title="添加分类" width="30%">
+            <el-form :model="categoryModel" :rules="rules" label-width="100px" style="padding-right: 30px">
+                <el-form-item label="分类名称" prop="categoryName">
+                    <el-input v-model="categoryModel.categoryName" minlength="1" maxlength="10"></el-input>
+                </el-form-item>
+                <el-form-item label="分类别名" prop="categoryAlias">
+                    <el-input v-model="categoryModel.categoryAlias" minlength="1" maxlength="15"></el-input>
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogVisible = false">取消</el-button>
+                    <el-button type="primary" @click="addArticleCategory()"> 确认 </el-button>
+                </span>
+            </template>
+        </el-dialog>
     </el-card>
 </template>
 
