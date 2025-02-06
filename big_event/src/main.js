9996a0408e3e2a8d035ef1f98a1198033043aae4
minedegithubhao <824737880@qq.com>
2025-02-06 21:11:34 +0800

    feat:使用element-plus中文语言包

--- big_event/src/main.js
+++ big_event/src/main.js
@@ -6,12 +6,13 @@ import App from './App.vue' // 引入App.vue
 import router from '@/router' // 引入router
 import { createPinia } from 'pinia'// 引入pinia
 import { createPersistedState } from 'pinia-persistedstate-plugin'//导入Pinia持久化插件-persist
+import locale from 'element-plus/dist/locale/zh-cn.js'//导入element-plus中文语言包
 
 const app = createApp(App) // 创建vue实例
 const pinia = createPinia() // 创建pinia实例
 const persist = createPersistedState() // 创建Pinia持久化插件-persist实例
 
-app.use(ElementPlus) // 使用element-plus
+app.use(ElementPlus, {locale}) // 使用element-plus及中文语言包
 app.use(router) // 使用router
 app.use(pinia)//使用pinia
 pinia.use(persist)//使用Pinia持久化插件-persist
