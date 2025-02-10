8bf7361ddc38ac93356cbe6bb770408fbd98f48b
minedegithubhao <824737880@qq.com>
2025-02-10 22:49:36 +0800

    feat:增加prifile和日志配置

--- big-event-springboot/src/main/java/org/example/BigEventSpringbootApplication.java
+++ big-event-springboot/src/main/java/org/example/BigEventSpringbootApplication.java
@@ -1,14 +1,23 @@
 package org.example;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BigEventSpringbootApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(BigEventSpringbootApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(BigEventSpringbootApplication.class, args);
+        logger.trace("trace logging");
+        logger.debug("debug logging");
+        logger.info("info logging");
+        logger.warn("warn logging");
+        logger.error("error logging");
     }
 
 }
